c2845dcfb518c5411b045a24977b8efb6e5e5ee5
Lilli Baaske <baaske@architrave.de>
2020-06-27 18:10:27 +0200

    Move state to component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
-/*jshint esversion: 8 */ 
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import db from './DB.json';
-import MitarbeiterListe from './MitarbeiterListe';
+/*jshint esversion: 8 */
 
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import db from "./DB.json";
+import Mitarbeiter from "./Mitarbeiter";
 
 function App() {
-
   const [liste, setListe] = useState([]);
-  const [menge, setMenge] = useState([0]);
 
-  useEffect(()=>{
+  useEffect(() => {
     getMitarbeiterListe();
-  }, [menge]);
-  
+  });
 
-  const getMitarbeiterListe = async () => {
-      let data = await db;
-      console.log(data);
-      setListe(data);
-      console.log('data:',data);
+  const getMitarbeiterListe = () => {
+    let data = db;
+    console.log(data);
+    setListe(data);
+    console.log("data:", data);
+  };
+
+  if (liste.length === 0) {
+    return null;
   }
+  const mitarbeiterliste = liste.map((mitarbeiter) => {
+    let uniqueId = Math.random();
+    return <Mitarbeiter mitarbeiter={mitarbeiter} key={uniqueId} />;
+  });
 
-  const updateSendungsmenge = async (e) => {
-    await setMenge(e.target.value);
-    console.log('menge:',menge);
-  }
-  
-
-  return (
-    <div className="App">
-      <h1>Das kannst Du!!</h1>
-      <MitarbeiterListe 
-      getMitarbeiterListe={getMitarbeiterListe} 
-      liste={liste} updateSendungsmenge={updateSendungsmenge} 
-      menge={menge}
-      setMenge={setMenge}
-      />
-    </div>
-  );
+  return <div className="App">{mitarbeiterliste}</div>;
 }
 
 export default App;
---
+++ src/Mitarbeiter.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Mitarbeiter = ({ mitarbeiter }) => {
+  const [menge, setMenge] = useState(0);
+
+  const updateSendungsmenge = (e) => {
+    setMenge(e.target.value);
+  };
+  console.log("menge:", menge);
+
+  const handleFocus = (e) => {
+    e.target.select();
+  };
+
+  return (
+    <div className="mitarbeiterCard">
+      <h1> {mitarbeiter.Name} </h1> <p> Anschrift: </p>
+      <p className="cardContent">
+        {mitarbeiter.Anschrift.Strasse} {mitarbeiter.Anschrift.Hausnummer}
+      </p>
+      <p>
+        {mitarbeiter.Anschrift.PLZ} {mitarbeiter.Anschrift.Stadt}
+      </p>
+      <p> Personalnummer: {mitarbeiter.Personalnummer} </p> <p> Zustellmittel: {mitarbeiter.Zustellmittel} </p>
+      <form onSubmit={updateSendungsmenge}>
+        <label htmlFor="Sendungsmenge"> Sendungsmenge: </label>
+        <input
+          type="number"
+          value={menge}
+          onFocus={handleFocus}
+          id="Sendungsmenge"
+          name="Sendungsmenge"
+          min="1"
+          max="1000"
+          onChange={updateSendungsmenge}
+        ></input>
+      </form>
+    </div>
+  );
+};
+export default Mitarbeiter;
